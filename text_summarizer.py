e0d2e1f984a17c5536aae4b312c9f3b3f9a2a469
Anindita-172 <82523510+Anindita-172@users.noreply.github.com>
2023-06-28 10:26:54 +0530

    defined inside a function

--- text_summarizer.py
+++ text_summarizer.py
@@ -11,47 +11,58 @@ Since Sanger's departure from Wikipedia, he has been critical of the project, de
 
 In 2006, he founded Citizendium to compete with Wikipedia. In 2010, he stepped down as editor-in-chief. In 2020, he left Citizendium entirely. In 2017, he joined Everipedia as chief information officer (CTO). He resigned in 2019, to establish the Knowledge Standards Foundation and the encyclosphere. Sanger currently serves as the President and Executive Director of the Knowledge Standards Foundation."""
 
-stopword = list(STOP_WORDS)
 
-nlp = spacy.load('en_core_web_sm')
-#print(nlp)
-doc = nlp(text)
-#print(doc)
-tokens = [token.text for token in doc]
-#print(tokens)
-word_freq = {}
-for word in doc:
-    if word.text.lower() not in stopword and word.text.lower() not in punctuation:
-        if word.text not in word_freq.keys():
-            word_freq[word.text] = 1
-        else:
-            word_freq[word.text]+=1
-#print(word_freq)
+def summarizer(rawdocs):
+    stopword = list(STOP_WORDS)
 
-
-max_freq = max(word_freq.values())
-#print(max_freq)
-
-for word in word_freq.keys():
-    word_freq[word] = word_freq[word]/max_freq
-
-#print(word_freq)
-
-sent_tokens = [sent for sent in doc.sents]
-#print(sent_tokens)
-
-sent_scores = {}
-for sent in sent_tokens:
-    for word in sent:
-        if word.text in word_freq.keys():
-            if sent not in sent_scores.keys():
-                sent_scores[sent] = word_freq[word.text]
+    nlp = spacy.load('en_core_web_sm')
+    #print(nlp)
+    doc = nlp(rawdocs)
+    #print(doc)
+    tokens = [token.text for token in doc]
+    #print(tokens)
+    word_freq = {}
+    for word in doc:
+        if word.text.lower() not in stopword and word.text.lower() not in punctuation:
+            if word.text not in word_freq.keys():
+                word_freq[word.text] = 1
             else:
-                sent_scores[sent] += word_freq[word.text]
-#print(sent_scores)
+                word_freq[word.text]+=1
+    #print(word_freq)
 
-select_len =int(len(sent_tokens)*0.3)
-#print(select_len)
 
-summary = nlargest(select_len , sent_scores , key= sent_scores.get)
-print(summary)
+    max_freq = max(word_freq.values())
+    #print(max_freq)
+
+    for word in word_freq.keys():
+        word_freq[word] = word_freq[word]/max_freq
+
+    #print(word_freq)
+
+    sent_tokens = [sent for sent in doc.sents]
+    #print(sent_tokens)
+
+    sent_scores = {}
+    for sent in sent_tokens:
+        for word in sent:
+            if word.text in word_freq.keys():
+                if sent not in sent_scores.keys():
+                    sent_scores[sent] = word_freq[word.text]
+                else:
+                    sent_scores[sent] += word_freq[word.text]
+    #print(sent_scores)
+
+    select_len =int(len(sent_tokens)*0.3)
+    #print(select_len)
+
+    summary = nlargest(select_len , sent_scores , key= sent_scores.get)
+    #print(summary)
+
+    final_summary = [word.text for word in summary]
+    summary = ' '.join(final_summary)
+    # print(summary)
+    # print(text)
+    # print("Length of original text", len(text.split( ' ')))
+    # print("Length of summary text", len(summary.split( ' ')))
+    
+    return summary, doc,len(rawdocs.split(' '),len(summary.split(' ')))
